a0b8d1ca4df5e89b123c183af247ad7d1738845c
chatlapin <choh9323@gmail.com>
2024-06-27 23:13:05 +0200

    reunion jun27
    ex1 fixed

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,13 +1,15 @@
 function pairNumbers(min, max) {
-    let result = "";
-    for (let i = min; i <= max; i += 2) {
-      if (i === max) {
-        result += i;
-      } else {
-        result += i + ",";
+  let result = "";
+  for (let i = min; i <= max; i++) {
+    if (i % 2 === 0) {
+      result += i 
+      if (i<max){
+        result+=','
+      }
       }
     }
-    return result;
-  }
-  
-  export default pairNumbers;
+  return result;
+}
+export default pairNumbers;
+
+
